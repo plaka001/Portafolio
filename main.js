ff71b66cf3c2a6ea1ca102b1c0755e8eb59323e1
Marlon cano <marloncano1997@gmail.com>
2023-08-06 14:48:12 -0500

    Styles add

---
+++ main.js
@@ -0,0 +1,5 @@
+// const btnChangeColor = document.getElementById("btn-change-color");
+
+// btnChangeColor.addEventListener("click", () => {
+//   document.documentElement.style.setProperty("--color-text", "red");
+// });
